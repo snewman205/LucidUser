82333ecafb34faa47f269a9c5fbc8d77af73cd83
Scott Newman <snewman205@icloud.com>
2013-01-08 03:59:44 -0500

    Updated logic in initialization method

--- models/theme.class.php
+++ models/theme.class.php
@@ -14,7 +14,9 @@ class Theme {
 	
 		global $Logging, $Localization;
 	
-		if(!$themeId) {
+		echo $themeId;
+	
+		if($themeId === NULL) {
 		
 			$Logging->error("Must specify theme ID", E_USER_ERROR);
 			//$Logging->error($Localization->stringFromKey(ERR_MISSING_THEMEID), E_USER_ERROR);
