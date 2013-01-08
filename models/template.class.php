3aa90c4065cdec05369d04eff3b49e5ec136b9d1
Scott Newman <snewman205@icloud.com>
2013-01-08 09:45:56 -0500

    Updated models/template.class.php to utilize the updated Theme class

--- models/template.class.php
+++ models/template.class.php
@@ -3,30 +3,32 @@
 require_once(LIB_DIR . "XTPL/xtemplate.class.php");
 
 class Template extends Theme {
-
+	
 	private $xtpl;
 
 	function Template($tpl = NULL) {
 		
-		global $Logging;
+		global $Logging, $Theme;
 		
 		if($tpl) {
 		
-			if(!file_exists($this->themePath . "views/" . $tpl)) {
+			$tplPath = $Theme->getPath() . "views/" . $tpl;
+		
+			if(!file_exists($tplPath)) {
 			
 				$Logging->error("Failed to open specified template file", E_USER_ERROR);
 				
 			}
 		
-			$this->xtpl = (!$this->xtpl) ? new XTemplate($tpl) : $this->xtpl->restart($tpl);
+			$this->xtpl = (!$this->xtpl) ? new XTemplate($tplPath) : $this->xtpl->restart($tplPath);
 			
 		}
 		
 	}
 	
-	function setTemplate($tpl = NULL) {
+	function set($tpl = NULL) {
 	
-		global $Logging;
+		global $Logging, $Theme;
 	
 		if($tpl === NULL) {
 		
@@ -34,13 +36,16 @@ class Template extends Theme {
 			
 		}
 		
-		if(!file_exists($this->themePath . "views/" . $tpl)) {
+		$tplPath = $Theme->getPath() . "views/" . $tpl;
+		
+		if(!file_exists($tplPath)) {
 			
+			echo $Theme->getPath() . "views/" . $tpl;
 			$Logging->error("Failed to open specified template file", E_USER_ERROR);
 				
 		}
 		
-		$this->xtpl = (!$this->xtpl) ? new XTemplate($tpl) : $this->xtpl->restart($tpl);
+		$this->xtpl = (!$this->xtpl) ? new XTemplate($tplPath) : $this->xtpl->restart($tplPath);
 		
 	}
 	
