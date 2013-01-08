b2cde2800bb78b2986f0dc61f8f305dab08cc728
Scott Newman <snewman205@icloud.com>
2013-01-08 08:16:24 -0500

    Updated models/core.php to implement MeekroDB; fetches current theme to initalize themeing class

--- models/core.php
+++ models/core.php
@@ -4,6 +4,7 @@ define("ROOT_DIR", dirname(__DIR__) . "/");
 define("MODEL_DIR", ROOT_DIR . "models/");
 define("LIB_DIR", ROOT_DIR . "libraries/");
 
+define("DB_PREFIX", "lu_");
 define("DB_HOST", "localhost");
 define("DB_NAME", "luciduser");
 define("DB_USER", "root");
@@ -11,16 +12,17 @@ define("DB_PASS", "");
 
 require_once(MODEL_DIR . "logging.class.php");
 require_once(MODEL_DIR . "localization.class.php");
-require_once(LIB_DIR . "dbFacile/dbFacile_mysqli.php");
+require_once(LIB_DIR . "MeekroDB/meekrodb.class.php");
 require_once(MODEL_DIR . "theme.class.php");
 
-$Database = new dbFacile_mysqli(DB_NAME, DB_USER, DB_PASS, DB_HOST);
+$Database = new MeekroDB(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 $Logging = new Logging(E_USER_ERROR);
 
 $Localization = new Localization();
 $Localization->setLang("EN");
 
-$Theme = new Theme();
+$currentTheme = $Database->queryOneField("configValue", "SELECT configValue FROM " . DB_PREFIX . "config WHERE configName=%s", "theme");
+$Theme = new Theme($currentTheme);
 
 ?>
