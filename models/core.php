a723d94c81cd144275546b3e5ffe5bc9dc3d4a1b
Scott Newman <snewman205@icloud.com>
2013-01-08 03:21:41 -0500

    Incorporated Propel DBAL in models/config.php

--- models/core.php
+++ models/core.php
@@ -4,9 +4,9 @@ define("ROOT_DIR", dirname(__DIR__) . "/");
 define("MODEL_DIR", ROOT_DIR . "models/");
 define("LIB_DIR", ROOT_DIR . "libraries/");
 
-require_once(LIB_DIR . "DABL/config.php");
 require_once(MODEL_DIR . "logging.class.php");
 require_once(MODEL_DIR . "localization.class.php");
+require_once(LIB_DIR . "Propel/Propel.php");
 require_once(MODEL_DIR . "theme.class.php");
 
 $Logging = new Logging(E_USER_ERROR);
