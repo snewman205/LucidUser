3956a61634185f312ea59e188f0a3e6e99d7b444
Scott Newman <snewman205@icloud.com>
2013-01-08 03:51:19 -0500

    Implemented dbFacile DBAL in models/core.php

--- models/core.php
+++ models/core.php
@@ -4,10 +4,17 @@ define("ROOT_DIR", dirname(__DIR__) . "/");
 define("MODEL_DIR", ROOT_DIR . "models/");
 define("LIB_DIR", ROOT_DIR . "libraries/");
 
+define("DB_HOST", "localhost");
+define("DB_NAME", "luciduser");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
 require_once(MODEL_DIR . "logging.class.php");
 require_once(MODEL_DIR . "localization.class.php");
-require_once(LIB_DIR . "Propel/Propel.php");
+require_once(LIB_DIR . "dbFacile/dbFacile_mysqli.php");
 require_once(MODEL_DIR . "theme.class.php");
+
+$Database = new dbFacile_mysqli(DB_NAME, DB_USER, DB_PASS, DB_HOST);
 
 $Logging = new Logging(E_USER_ERROR);
 
